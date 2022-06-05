185c10cef3c68a2a94c15b24128474294eb38322
v.shitikov <v.shitikov14@gmail.com>
2022-06-05 22:48:05 +0300

    diagonal fix

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,6 +1,6 @@
 function makeDiagonalRed(table) {
-  let getTable = document.body.firstElementChild;
-  for (let i = 0; i < getTable.rows.length; i++) {
+  let table = document.body.firstElementChild;
+  for (let i = 0; i < table.rows.length; i++) {
     let row = table.rows[i];
     row.cells[i].style.backgroundColor = 'red';
   }
