e9304ab989ddaf762d2e818e3d7a84898592e3b1
v.shitikov <v.shitikov14@gmail.com>
2022-06-08 14:01:13 +0300

    hide self

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,3 +1,6 @@
 function hideSelf() {
-  // ваш код...
+  let buttonHide = document.querySelector('.hide-self-button');
+  buttonHide.onclick = function() {
+    buttonHide.setAttribute('hidden', 1);
+  }
 }
