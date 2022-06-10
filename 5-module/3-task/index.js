76cd42b719438182fbd07f721043057850bf7791
v.shitikov <v.shitikov14@gmail.com>
2022-06-10 18:41:15 +0300

    slider

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,38 @@
 function initCarousel() {
-  // ваш код...
+  const arrowLeft = document.querySelector('.carousel__arrow_left');
+  const arrowRight = document.querySelector('.carousel__arrow_right');
+
+  const containerSlides = document.querySelector('.carousel__inner');
+  const slides = containerSlides.querySelectorAll('.carousel__slide');
+  const slidesCount = slides.length;
+  console.log(slidesCount);
+
+  const widthSlide = containerSlides.offsetWidth;
+
+  let count = 0;
+  let currentIndex = 0;
+
+  arrowLeft.style.display = 'none';
+
+  arrowRight.addEventListener('click', function() {
+    count += widthSlide;
+    currentIndex++;
+    console.log(currentIndex);
+    containerSlides.style.transform = `translateX(-${count}px)`;
+    if (currentIndex >= slidesCount - 1) {
+      arrowRight.style.display = 'none';
+    }
+    arrowLeft.style.display = 'flex';
+  });
+
+
+  arrowLeft.addEventListener('click', function() {
+    count -= widthSlide;
+    currentIndex--;
+    containerSlides.style.transform = `translateX(-${count}px)`;
+    if (currentIndex <= 0) {
+      arrowLeft.style.display = 'none';
+    }
+    arrowRight.style.display = 'flex';
+  });
 }
