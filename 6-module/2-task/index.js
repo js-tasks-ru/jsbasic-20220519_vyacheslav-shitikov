d2af53fbf7d36c8ab819b1342262329e02740a96
v.shitikov <v.shitikov14@gmail.com>
2022-06-17 18:18:06 +0300

    user table fix

--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,4 +1,20 @@
 export default class ProductCard {
   constructor(product) {
+    this.product = product;
   }
+  // createHTML() {
+  //   return `<div class="card">
+  //       <div class="card__top">
+  //         <img src="/assets/images/icons/" class="card__image" alt="product">
+  //         <span class="card__price"></span>
+  //       </div>
+  //       <div class="card__body">
+  //         <div class="card__title"></div>
+  //         <button type="button" class="card__button">
+  //           <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+  //         </button>
+  //       </div>
+  //     </div>
+  //     `
+  // }
 }
